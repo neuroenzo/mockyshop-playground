e2869eee9a09c0f55ede41f0c4e2a608bf385565
neuroenzo <neuroenzo@gmail.com>
2026-06-06 09:40:52 +0300

    rename users route

--- mockyshop/backend/app/routers/users.py
+++ mockyshop/backend/app/routers/users.py
@@ -55,7 +55,7 @@ async def get_current_user_profile(
     return current_user
 
 
-@router.post("/token")
+@router.post("/login")
 async def login(
     form_data: OAuth2PasswordRequestForm = Depends(),
     db: AsyncSession = Depends(get_async_db),
